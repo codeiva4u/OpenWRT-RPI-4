65da8512834fac9b500a426cebb484185f9a2006
codeiva4u <codeiva@protonmail.com>
2025-08-04 21:50:06 +0530

    Final Fix: Switch to direct package clone for maximum reliability

--- diy-part1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-
-# Add a feed source for Argon theme
-echo 'src-git jerrykuku https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
