b9a57b810f5d577c0aa637d725e13227a347008f
codeiva4u <codeiva@protonmail.com>
2025-08-04 21:13:35 +0530

    Configure OpenWrt for Raspberry Pi 4 with custom features

--- files/etc/uci-defaults/20-theme-setup.sh
+++ files/etc/uci-defaults/20-theme-setup.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-
-# Set Argon as the default theme for LuCI
 uci set luci.main.mediaurlbase='/luci-static/argon'
 uci commit luci
-
-# Clean up the script itself after execution
-rm -- "$0"
-
 exit 0
--- files/etc/uci-defaults/30-rootfs-expand.sh
+++ files/etc/uci-defaults/30-rootfs-expand.sh
@@ -1,49 +1,12 @@
 #!/bin/sh
-
-# This script expands the root partition to fill the SD card.
-# It is designed for Raspberry Pi devices.
-
-# Only run once
-if [ -f /etc/rootfs_expanded ]; then
-    exit 0
+if [ ! -f /etc/rootfs_expanded ]; then
+    ROOT_PART=$(findmnt / -o SOURCE -n)
+    ROOT_DEV=${ROOT_PART%?}
+    PART_NUM=${ROOT_PART#$ROOT_DEV}
+    umount / || true
+    (echo d; echo $PART_NUM; echo n; echo p; echo $PART_NUM; echo; echo; echo w) | fdisk $ROOT_DEV
+    touch /etc/rootfs_expanded
+    echo "Root partition expanded. Rebooting now..."
+    reboot
 fi
-
-# Get the root device and partition number
-ROOT_DEV=$(findmnt / -o source -n)
-PART_NUM=$(echo ${ROOT_DEV} | grep -o '[0-9]*$')
-DISK=$(echo ${ROOT_DEV} | sed -e "s/${PART_NUM}$//")
-
-# Check if we are on a valid mmcblk device
-if ! echo "${DISK}" | grep -q "mmcblk"; then
-    echo "Not running on a recognized SD card (mmcblk). Aborting."
-    exit 1
-fi
-
-echo "Expanding ${ROOT_DEV} on disk ${DISK}..."
-
-# Use fdisk to delete and recreate the partition with a larger size
-# This is safe as long as we don't touch the start sector.
-START_SECTOR=$(fdisk -l ${DISK} | grep ${ROOT_DEV} | awk '{print $2}')
-
-# Unmount the partition temporarily if possible (might not be needed)
-# umount -l ${ROOT_DEV}
-
-# Recreate the partition
-fdisk ${DISK} <<EOF
-d
-${PART_NUM}
-n
-p
-${PART_NUM}
-${START_SECTOR}
-
-w
-EOF
-
-echo "Partition resized. Rebooting to apply changes..."
-
-# Mark that the expansion is done so it doesn't run again
-touch /etc/rootfs_expanded
-
-# Reboot to allow the kernel to see the new partition size
-reboot
+exit 0
