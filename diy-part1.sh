c8158f050b69012fd9575de8be1abc35ad127fc4
codeiva4u <codeiva@protonmail.com>
2025-08-04 21:23:06 +0530

    Feat: Overhaul build process to use P3TERX action

---
+++ diy-part1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+
+# Add a feed source for Argon theme
+echo 'src-git jerrykuku https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
